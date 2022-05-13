d91202fc4745f897e159a3927b8b5457c5574374
JoePerez11 <perez.joseph.k@gmail.com>
2022-05-13 11:22:48 -0500

    added edit button functionality

--- movie-project.js
+++ movie-project.js
@@ -51,15 +51,15 @@ function makeTable(response) {
 function makeTableEdits(response) {
     let edits = "<tr>";
     response.forEach((obj) => {
-        edits += `<th id="${obj.id}" scope="row"><form><textarea style="overflow-wrap: normal" class="form-control" placeholder="${obj.id}"></textarea></form></th>`;
-        edits += `<td id="poster-${obj.id}"></td>`;
-        edits += `<td id="title-${obj.id}" ><form><textarea style="overflow-wrap: normal" class="form-control" placeholder="${obj.title}"></textarea></form></td>`;
-        edits += `<td id="genre-${obj.id}" ><form><textarea style="overflow-wrap: normal" class="form-control" placeholder="${obj.genre}"></textarea></form></td>`;
-        edits += `<td id="year-${obj.id}" ><form><textarea style="overflow-wrap: normal" class="form-control text-center" placeholder="${obj.year}"></textarea></form></td>`
-        edits += `<td id="rating-${obj.id}" ><form><textarea style="overflow-wrap: normal" class="form-control text-center" placeholder="${obj.rating}"></textarea></form></td>`
-        edits += `<td id="plot-${obj.id}" ><form><textarea style="overflow-wrap: normal" class="form-control" placeholder="${obj.plot}"></textarea></form></td>`;
+        edits += `<th  scope="row"><form><textarea id="${obj.id}" style="overflow-wrap: normal" class="form-control" placeholder="${obj.id}"></textarea></form></th>`;
+        edits += `<td><form><textarea id="poster-${obj.id}" style="overflow-wrap: normal" class="form-control" ></textarea></form></td>`;
+        edits += `<td><form><textarea id="title-${obj.id}" style="overflow-wrap: normal" class="form-control" placeholder="${obj.title}"></textarea></form></td>`;
+        edits += `<td ><form><textarea id="genre-${obj.id}" style="overflow-wrap: normal" class="form-control" placeholder="${obj.genre}"></textarea></form></td>`;
+        edits += `<td ><form><textarea id="year-${obj.id}" style="overflow-wrap: normal" class="form-control text-center" placeholder="${obj.year}"></textarea></form></td>`
+        edits += `<td ><form><textarea id="rating-${obj.id}" style="overflow-wrap: normal" class="form-control text-center" placeholder="${obj.rating}"></textarea></form></td>`
+        edits += `<td ><form><textarea id="plot-${obj.id}" style="overflow-wrap: normal" class="form-control" placeholder="${obj.plot}"></textarea></form></td>`;
         edits += `<td>
-                    <button type="button" id="submit-${obj.id}" class="btn btn-primary mb-1 w-100">Submit</button>
+                    <button type="button" id="submit-${obj.id}" class="btn btn-success submit-btn mb-1 w-100">Submit</button>
                     <button type="button" id="delete-${obj.id}" class="btn btn-danger w-100">Delete</button>
                  </td>`;
         edits += "</tr>";
@@ -113,19 +113,20 @@ $(document).on('click', '.btn-danger', function(e){
 //      Like creating a movie, this should not involve any page reloads, instead your javascript code should make an ajax request when the form is submitted.
 // =============== REQUIRED ACTION ITEMS REMAINING ==========================
 
-$(document).on('click', '.btn-primary', function(e){
+$(document).on('click', '.btn-primary', function(e) {
     e.preventDefault();
     let uniqueID = $(e.target).attr("id");
     // TODO: Need a fn like makeTable that creates text boxes populated with current movie content in the row that the edit button was clicked
     // TODO: Edit button should convert to a Submit button
     // TODO: Capture the data from all of the new forms in each column
     // TODO: Check the PATCH request below to ensure the newly captured data is being sent to the database.
-    fetch(url).then(response => {response.json()
-        .then(result => {
-            console.log("Render Movies: ", result);
-            makeTableEdits(result);
-        })
-    }) .catch(error => console.error(error));
+    fetch(url).then(response => {
+        response.json()
+            .then(result => {
+                console.log("Render Movies: ", result);
+                makeTableEdits(result);
+            })
+    }).catch(error => console.error(error));
 }
     // const patchOption = {
     //     method: 'PATCH',
@@ -134,5 +135,30 @@ $(document).on('click', '.btn-primary', function(e){
     //     },
     // };
     // let patchURL = url + "/" + uniqueID;
-    // fetch(patchURL, patchOption).then( success => getMovies());
+    // fetch4(patchURL, patchOption).then( success => getMovies());
 );
+
+
+$(document).on('click', '.btn-success', function(e) {
+    e.preventDefault();
+    let uniqueID = $(e.target).attr("id");
+    let editID = uniqueID.substring(7, uniqueID.length);
+    const editMoviePost = {
+        genre: $(`#genre-${editID}`).val(),
+        id: $(`#id-${editID}`).val(),
+        plot: $(`#plot-${editID}`).val(),
+        poster: $(`#poster-${editID}`).val(),
+        title: $(`#title-${editID}`).val(),
+        rating: $(`#rating-${editID}`).val()
+    };
+    console.log(editMoviePost);
+    const patchOption = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editMoviePost),
+    };
+    let deleteURL = url + "/" + uniqueID.substring(7, uniqueID.length);
+    fetch(deleteURL, patchOption).then( success => getMovies());
+})
